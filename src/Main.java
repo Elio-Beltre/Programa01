bdf9ecb74175f4acfaa5e9167fa6d6760a3e903f
Laboratorio 109 Pc-15 <lab-109-inf-15>
2023-09-13 21:29:05 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //1- identicar variables
+        int a, b, total;
+        Scanner S = new Scanner(System.in);
+
+        //2- imprimir el mensaje "ingrese a"
+        System.out.println("ingrese a");
+
+
+        //3- leer y almacenar en variable a
+        a = S.nextInt();
+
+        //4- imprimir el mensaje "ingrese b"
+        System.out.println("Ingrese b");
+
+        //5- leer y almacenar en variable b
+        b= S.nextInt();
+import java.util.Scanner;
+
+        public class Main {
+            public static void main(String[] args) {
+                //1- identicar variables
+                int a, b, total;
+                Scanner S = new Scanner(System.in);
+
+                //2- imprimir el mensaje "ingrese a"
+                System.out.println("ingrese a");
+
+
+                //3- leer y almacenar en variable a
+                a = S.nextInt();
+
+                //4- imprimir el mensaje "ingrese b"
+                System.out.println("Ingrese b");
+
+                //5- leer y almacenar en variable b
+                b= S.nextInt();
+
+                //6- total = a* b
+                total = a*b;
+
+                //7- imprimir el mensaje "el resultado es "
+                // al valor del total
+                System.out.println("El resultado es " + total);
+            }
+
+        //6- total = a* b
+            total = a*b;
+
+        //7- imprimir el mensaje "el resultado es "
+        // al valor del total
+        System.out.println("El resultado es " + total);
+    }
+}
