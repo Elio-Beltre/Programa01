6573e2803659492c182725897d50384b5391c517
Elio-Beltre <144402917+Elio-Beltre@users.noreply.github.com>
2023-09-17 18:06:13 -0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,51 +7,22 @@ public class Main {
         Scanner S = new Scanner(System.in);
 
         //2- imprimir el mensaje "ingrese a"
-        System.out.println("ingrese a");
-
+        System.out.print("ingrese a: ");
 
         //3- leer y almacenar en variable a
         a = S.nextInt();
 
         //4- imprimir el mensaje "ingrese b"
-        System.out.println("Ingrese b");
+        System.out.print("ingrese b: ");
 
         //5- leer y almacenar en variable b
         b= S.nextInt();
-import java.util.Scanner;
-
-        public class Main {
-            public static void main(String[] args) {
-                //1- identicar variables
-                int a, b, total;
-                Scanner S = new Scanner(System.in);
-
-                //2- imprimir el mensaje "ingrese a"
-                System.out.println("ingrese a");
-
-
-                //3- leer y almacenar en variable a
-                a = S.nextInt();
-
-                //4- imprimir el mensaje "ingrese b"
-                System.out.println("Ingrese b");
-
-                //5- leer y almacenar en variable b
-                b= S.nextInt();
-
-                //6- total = a* b
-                total = a*b;
-
-                //7- imprimir el mensaje "el resultado es "
-                // al valor del total
-                System.out.println("El resultado es " + total);
-            }
 
         //6- total = a* b
-            total = a*b;
+        total = a*b;
 
         //7- imprimir el mensaje "el resultado es "
         // al valor del total
-        System.out.println("El resultado es " + total);
+        System.out.print("El resultado es " + total);
     }
 }
